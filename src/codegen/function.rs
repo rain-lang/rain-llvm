414cb62a1a07fc14591ed8755bede2fb55066ca8
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-26 00:27:39 -0400

    Tuple compilation

--- src/codegen/function.rs
+++ src/codegen/function.rs
@@ -1,13 +1,15 @@
 /*!
-Code generations for rain functions
+Code generation for rain functions
 */
 use super::*;
 use inkwell::values::FunctionValue;
 use rain_ir::function::pi::Pi;
 
 impl<'ctx> Codegen<'ctx> {
+
     /// Compile a constant `rain` function
     pub fn compile_constant(&mut self, _ty: &Pi, _val: &ValId) -> FunctionValue<'ctx> {
         unimplemented!()
     }
+
 }
--- src/lib.rs
+++ src/lib.rs
@@ -254,53 +254,6 @@ impl<'ctx> Codegen<'ctx> {
         }
     }
 
-    /// Compile a product
-    pub fn compile_product(&mut self, p: &Product) -> Result<Repr<'ctx>, Error> {
-        let mut mapping: Vec<Option<u32>> = Vec::new();
-        let mut struct_index = 0;
-        let mut repr_vec: Vec<BasicTypeEnum<'ctx>> = Vec::new();
-        let mut reprs = p.iter().map(|ty| self.get_repr(ty));
-        while let Some(repr) = reprs.next() {
-            let repr = repr?;
-            match repr {
-                Repr::Type(ty) => {
-                    repr_vec.push(ty);
-                    mapping.push(Some(struct_index));
-                    struct_index += 1;
-                }
-                Repr::Function(f) => {
-                    repr_vec.push(f.ptr_type(AddressSpace::Global).into());
-                    mapping.push(Some(struct_index));
-                    struct_index += 1;
-                }
-                Repr::Empty => return Ok(Repr::Empty),
-                Repr::Irrep => {
-                    let mut return_empty = false;
-                    while let Some(r) = reprs.next() {
-                        if r? == Repr::Empty {
-                            return_empty = true;
-                        }
-                    }
-                    if return_empty {
-                        return Ok(Repr::Empty);
-                    }
-                }
-                Repr::Prop => mapping.push(None),
-                Repr::Product(p) => {
-                    repr_vec.push(p.repr.into());
-                    mapping.push(Some(struct_index));
-                    struct_index += 1;
-                }
-            }
-        }
-        if struct_index == 0 {
-            Ok(Repr::Empty)
-        } else {
-            let repr = self.context.struct_type(&repr_vec[..], false);
-            Ok(Repr::Product(ProductRepr { mapping, repr }))
-        }
-    }
-
     /// Compile a tuple
     // pub fn compile_tuple(&mut self, t: &Tuple) -> Result<Const<'ctx>, Error> {
     //     // let p = t.ty().clone_ty().as_enum();
@@ -352,66 +305,6 @@ impl<'ctx> Codegen<'ctx> {
             let c = self.compile_enum(v.as_enum())?;
             self.consts.insert(v.clone(), c);
             Ok(c)
-        }
-    }
-
-    /// Build a parameter in a local context
-    pub fn build_parameter(
-        &mut self,
-        ctx: &mut LocalCtx<'ctx>,
-        p: &Parameter,
-    ) -> Result<Local<'ctx>, Error> {
-        ctx.locals
-            .get(&ValId::from(p.clone()))
-            .cloned()
-            .ok_or(Error::InternalError(
-                "Context should have parameters pre-registered!",
-            ))
-    }
-
-    /// Build a tuple in a local context
-    pub fn build_tuple(
-        &mut self,
-        ctx: &mut LocalCtx<'ctx>,
-        p: &Tuple,
-    ) -> Result<Local<'ctx>, Error> {
-        let p_enum = p.ty().as_enum();
-        match p_enum {
-            ValueEnum::Product(product) => {
-                let repr = match self.compile_product(product)? {
-                    Repr::Product(tmp) => tmp,
-                    Repr::Prop => return Ok(Local::Unit),
-                    Repr::Empty => return Ok(Local::Contradiction),
-                    // TODO: think about Local::Irrep
-                    Repr::Irrep => return Err(Error::Irrepresentable),
-                    // TODO: Rethink the following later
-                    Repr::Function(_f) => {
-                        return Err(Error::NotImplemented("Function in tuple not implemented"));
-                    }
-                    Repr::Type(_t) => {
-                        return Err(Error::NotImplemented("Type in tuple not supported yet"))
-                    }
-                };
-                let mut values: Vec<BasicValueEnum<'ctx>> = Vec::new();
-                for (i, mapped) in repr.mapping.iter().enumerate() {
-                    if let Some(_mapped_pos) = mapped {
-                        let this_result = self.build(ctx, &p[i])?;
-                        // Note: This assumes that each type has unique representation
-                        let value: BasicValueEnum<'ctx> = match this_result {
-                            Local::Value(v) => match v.try_into() {
-                                Ok(v) => v,
-                                Err(()) => unimplemented!("Function types in tuple"),
-                            },
-                            l => panic!("Invalid struct member {:?}", l),
-                        };
-                        values.push(value);
-                    }
-                }
-                Ok(Local::Value(
-                    repr.repr.const_named_struct(&values[..]).into(),
-                ))
-            }
-            _ => Err(Error::InternalError("Expected a product")),
         }
     }
 
