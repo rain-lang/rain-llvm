57db15e28ea4372960ab88727d5ab129835db196
Qingyuan Qie <will.qie@mail.utoronto.ca>
2020-07-11 16:31:32 -0400

    clean up

--- src/codegen/function.rs
+++ src/codegen/function.rs
@@ -10,19 +10,6 @@ use rain_ir::region::Regional;
 use rain_ir::typing::Typed;
 use rain_ir::value::expr::Sexpr;
 
-/// A prototype for a `rain` function
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum Prototype<'ctx> {
-    /// An LLVM function value
-    Function(FunctionValue<'ctx>),
-    /// A unit function, i.e. a function whose return type is a unit type or contradiction
-    /// *or* which has a contradiction as an argument
-    Unit,
-    /// An irrepresentable function, which has irrepresentable arguments and a non unit/contradiction
-    /// return type *and* no contradiction arguments
-    Irrep,
-}
-
 /// The default linkage of lambda values
 pub const DEFAULT_LAMBDA_LINKAGE: Option<Linkage> = None;
 
@@ -32,118 +19,6 @@ impl<'ctx> Codegen<'ctx> {
         unimplemented!()
     }
 
-    // /// Create a function prototype for a lambda function, binding its parameters
-    // pub fn build_prototype(&mut self, lambda: &Lambda) -> Result<Prototype<'ctx>, Error> {
-    //     if lambda.depth() != 0 {
-    //         unimplemented!(
-    //             "Closures not implemented for lambda {} (depth = {})!",
-    //             lambda,
-    //             lambda.depth()
-    //         )
-    //     }
-    //     let pi = lambda.get_ty();
-    //     let region = pi.def_region();
-    //     let result = pi.result();
-    //     if result.depth() != 0 {
-    //         return Err(Error::NotImplemented(
-    //             "Non-constant return types for pi functions",
-    //         ));
-    //     }
-    //     let result_repr = match self.repr(result)? {
-    //         Repr::Type(t) => t,
-    //         Repr::Function(_f) => unimplemented!(),
-    //         Repr::Empty | Repr::Prop => return Ok(Prototype::Unit),
-    //         Repr::Irrep => return Ok(Prototype::Irrep),
-    //         Repr::Product(p) => p.repr.into(),
-    //     };
-    //     let mut input_reprs: Vec<BasicTypeEnum> = Vec::with_capacity(region.len());
-    //     let mut input_ixes: Vec<isize> = Vec::with_capacity(region.len());
-    //     const PROP_IX: isize = -1;
-    //     const IRREP_IX: isize = -2;
-    //     let mut has_empty = false;
-
-    //     for input_ty in region.iter() {
-    //         match self.repr(input_ty)? {
-    //             Repr::Type(t) => {
-    //                 if !has_empty {
-    //                     input_ixes.push(input_reprs.len() as isize);
-    //                     input_reprs.push(t);
-    //                 }
-    //             }
-    //             Repr::Function(_) => unimplemented!(),
-    //             Repr::Prop => {
-    //                 if !has_empty {
-    //                     input_ixes.push(PROP_IX);
-    //                 }
-    //             }
-    //             Repr::Empty => has_empty = true,
-    //             Repr::Irrep => {
-    //                 if !has_empty {
-    //                     input_ixes.push(IRREP_IX);
-    //                 }
-    //             }
-    //             Repr::Product(p) => {
-    //                 if !has_empty {
-    //                     input_ixes.push(input_reprs.len() as isize);
-    //                     input_reprs.push(p.repr.into());
-    //                 }
-    //             }
-    //         }
-    //     }
-
-    //     if has_empty {
-    //         return Ok(Prototype::Unit);
-    //     }
-
-    //     // Construct a function type
-    //     let result_ty = result_repr.fn_type(&input_reprs, false);
-
-    //     // Construct an empty function of a given type
-    //     let result_fn = self.module.add_function(
-    //         &format!("__lambda_{}", self.counter),
-    //         result_ty,
-    //         DEFAULT_LAMBDA_LINKAGE,
-    //     );
-    //     self.counter += 1;
-
-    //     self.curr_ix = self.local_arena.push(SymbolTable::default());
-    //     self.local_ixs.insert(result_fn, (self.curr_ix, None));
-
-    //     let this_table = match self.local_arena.get_mut(self.curr_ix) {
-    //         Some(t) => t,
-    //         None => panic!("A symbol table should be pushed when building a prototype"),
-    //     };
-    //     // Bind parameters
-    //     for (i, ix) in input_ixes.iter().copied().enumerate() {
-    //         let valid = ValId::from(
-    //             region
-    //                 .clone()
-    //                 .param(i)
-    //                 .expect("Iterated index is in bounds"),
-    //         );
-    //         match ix {
-    //             PROP_IX => {
-    //                 this_table.insert(valid, Val::Unit);
-    //             }
-    //             IRREP_IX => {
-    //                 this_table.insert(valid, Val::Irrep);
-    //             }
-    //             ix => {
-    //                 this_table.insert(
-    //                     valid,
-    //                     Val::Value(
-    //                         result_fn
-    //                             .get_nth_param(ix as u32)
-    //                             .expect("Index in vector is in bounds")
-    //                             .into(),
-    //                     ),
-    //                 );
-    //             }
-    //         }
-    //     }
-
-    //     Ok(Prototype::Function(result_fn))
-    // }
     /// Build a function call with arguments
     pub fn build_function_call(
         &mut self,
@@ -336,12 +211,6 @@ impl<'ctx> Codegen<'ctx> {
 
         self.curr_ix = self.local_arena.push(SymbolTable::default());
         self.local_ixs.insert(result_fn, (self.curr_ix, None));
-
-        // let this_table = match self.local_arena.get_mut(self.curr_ix) {
-        //     Some(t) => t,
-        //     None => panic!("A symbol table should be pushed when building a prototype"),
-        // };
-
         let mut parameter_values: Vec<Val<'ctx>> = Vec::with_capacity(region.len());
         // Bind parameters
         for ix in input_ixes.iter().copied() {
@@ -376,8 +245,7 @@ impl<'ctx> Codegen<'ctx> {
             (self.curr_ix, Some(entry_bb))
         );
         self.builder.position_at_end(entry_bb);
-        // Build a return value for the current function
-        // let retv = self.build(lambda.result());
+        // Build the result of this lambda
         let retv = self.build_lambda_inline(lambda, &parameter_values[..]);
         // If successful, build a return instruction
         let retv_build = match retv {
