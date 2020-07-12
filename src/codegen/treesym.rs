d6b72bec58cf8e28c38c5fb5c8d9171d27bfde58
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-07-12 19:24:30 -0400

    Start implementation

--- src/codegen/treesym.rs
+++ src/codegen/treesym.rs
@@ -1,3 +1,11 @@
 /*!
-A symbol table implemented with hash trees
+A symbol table implemented with hash trees, supporting saving snapshots
 */
+use im::HashMap;
+use std::hash::Hash;
+
+/// A symbol table implemented with hash trees, supporting mixed levels
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct SymbolTable<K: Eq + Hash, V> {
+    levels: Vec<HashMap<K, V>>
+}
